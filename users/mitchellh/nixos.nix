eddf8520c8fcdb1554d6974e1f79d24d1732ad9a
qzjk20 <gongqian.li@gm.com>
2023-10-11 08:37:09 -0400

    update utm config

--- users/mitchellh/nixos.nix
+++ users/mitchellh/nixos.nix
@@ -15,7 +15,7 @@
     home = "/home/mitchellh";
     extraGroups = [ "docker" "wheel" ];
     shell = pkgs.fish;
-    hashedPassword = "$6$p5nPhz3G6k$6yCK0m3Oglcj4ZkUXwbjrG403LBZkfNwlhgrQAqOospGJXJZ27dI84CbIYBNsTgsoH650C1EBsbCKesSVPSpB1";
+    hashedPassword = "$6$ZhP6W99a3mU$z8SCXJ7WUcisIbcxryOTyXAmMZj0RMdCqcaUxNsPIHtdXg6kI4DOT3nomi.NdK2UKbqBuoOAT43vpt.8FIPvM/";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGbTIKIPtrymhvtTvqbU07/e7gyFJqNS4S0xlfrZLOaY mitchellh"
     ];
